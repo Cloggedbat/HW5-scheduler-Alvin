746e26f5f232132bd90663518c1e8b6c8dd1bb12
cleminator <alvinclemens@gmail.com>
2020-08-19 21:16:25 -0600

    second push

--- Java.js
+++ Java.js
@@ -240,8 +240,32 @@ document.getElementById("saveBtn9").addEventListener("click", function () {
     console.log("click")
 }, false);
 
+let newArray = [];
+let time = momment();
+let timeFormat = "hh:mma"
+let eightStart = moment('8:00am',timeFormat)
+let eightEnd = moment('8:59am',timeFormat)
+let nineStart = moment('9:00am',timeFormat)
+let nineEnd = moment('9:59am',timeFormat)
+let tenStart = moment('10:00am',timeFormat)
+let tenEnd = moment('10:59am',timeFormat)
+let elevenStart = moment('11:00am',timeFormat)
+let elevenend = moment('11:59am',timeFormat)
+let twelveStart = moment('12:00am',timeFormat)
+let oneStart = moment('1:00pm',timeFormat)
+let oneEnd = moment('1:59pm',timeFormat)
+let twoStart = moment('2:00pm',timeFormat)
+let twoEnd = moment('2:59pm',timeFormat)
+let threeStart = moment('3:00pm',timeFormat)
+let threeEnd = moment('3:59pm',timeFormat)
+let fourStart = moment('4:00pm',timeFormat)
+let fourEnd = moment('4:59pm',timeFormat)
+let fiveStart = moment('5:00pm',timeFormat)
+let fiveEnd = moment('5:59pm',timeFormat)
 
-
+setInterval(function(){
+    checkTime();
+},1000);
 
 
 // iso string

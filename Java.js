da74075654840fe1e37e8a569b2a0b1a0e6c3cd3
cleminator <alvinclemens@gmail.com>
2020-08-22 17:23:01 -0600

    final go

--- Java.js
+++ Java.js
@@ -1,115 +1,80 @@
 $(document).ready(function () {
 
 
-// curent date + time
-// const m = moment();
-// const m2 = moment()
-//  const now = new Date(year, month, day);
-// document.getElementById('test').innerHTML = date;
-var NowMoment = moment();
-var eDisplayMoment = document.getElementById('test');
-eDisplayMoment.innerHTML = NowMoment.format('M-D-YYYY');
+    // curent date + time
+    // const m = moment();
+    // const m2 = moment()
+    //  const now = new Date(year, month, day);
+    // document.getElementById('test').innerHTML = date;
+    var NowMoment = moment();
+    var eDisplayMoment = document.getElementById('test');
+    eDisplayMoment.innerHTML = NowMoment.format('M-D-YYYY');
 
-// const saveTask = (ev)=>{
-//   ev.preventDefault();  
-//   let save = 
-// }
-// moment().format('MMMM Do YYYY, h:mm:ss a');
+    // Target an element using moment 
 
-// document.addEventListener("DOMContentLoaded", ()=>{
-//     document.getElementById('btn').addEventListener('click', saveTask)
-// })
+    // const saveTask = (ev)=>{
+    //   ev.preventDefault();  
+    //   let save = 
+    // }
+    // moment().format('MMMM Do YYYY, h:mm:ss a');
 
-// $("#saveBtn").on("click",function(){
-//    console.log("click")
-// });
+    // document.addEventListener("DOMContentLoaded", ()=>{
+    //     document.getElementById('btn').addEventListener('click', saveTask)
+    // })
 
-function wawa(buttonClassName, textAreaClassName, keyName){
-    $(buttonClassName).on("click", function () {
-        // get text from wawa
-        var textarea = $(textAreaClassName).val();
+    // $("#saveBtn").on("click",function(){
+    //    console.log("click")
+    // });
 
-        localStorage.setItem(keyName, textarea)
-    })
-    
-    var save = localStorage.getItem(keyName)
-    console.log(save)
-    $(textAreaClassName).val(save);
+    function wawa(thiswillsave, funbox, keyName) {
+        $(thiswillsave).on("click", function () {
+            // get text from wawa 
+            var textarea = $(funbox).val();
+            // text area is saved in local storage
+            localStorage.setItem(keyName, textarea)
+        })
+        // data is retreved and displayed in textarea
+        var save = localStorage.getItem(keyName)
+        $(funbox).val(save);
+    }
+    // wawa is pulling text/saveBtn activates/user-... is targeted for data... insertion
+    wawa("#saveBtn", ".user-1", "data10")
+    wawa("#saveBtn1", ".user-2", "data9")
+    wawa("#saveBtn2", ".user-3", "data8")
+    wawa("#saveBtn3", ".user-4", "data7")
+    wawa("#saveBtn4", ".user-5", "data6")
+    wawa("#saveBtn5", ".user-6", "data5")
+    wawa("#saveBtn6", ".user-7", "data4")
+    wawa("#saveBtn7", ".user-8", "data3")
+    wawa("#saveBtn8", ".user-9", "data2")
+    wawa("#saveBtn9", ".user-10", "data1")
+
+
+var textareas = $(".form-control")
+console.log(textareas)
+
+for (var i = 0; i < textareas.length; i++){ 
+
+var currentTextarea = $(textareas[i])
+
+let textareahour = currentTextarea.attr("id");
+let currenthour = NowMoment.format('H')
+console.log(textareahour, currenthour, currentTextarea)
+
+if (currenthour > parseInt(textareahour)){
+    currentTextarea.addClass("past")
+} else if (currenthour == parseInt(textareahour)){
+    currentTextarea.addClass("present")
+} else if (currenthour < parseInt(textareahour)){
+    currentTextarea.addClass("future")
 }
-wawa("#saveBtn", ".user-1", "data10")
-wawa("#saveBtn1", ".user-2", "data9")
-wawa("#saveBtn2", ".user-3", "data8")
-wawa("#saveBtn3", ".user-4", "data7")
-wawa("#saveBtn4", ".user-5", "data6")
-wawa("#saveBtn5", ".user-6", "data5")
-wawa("#saveBtn6", ".user-7", "data4")
-wawa("#saveBtn7", ".user-8", "data3")
-wawa("#saveBtn8", ".user-9", "data2")
-wawa("#saveBtn9", ".user-10", "data1")
 
-// if (localStorage["user"]) {
-//     var user = localStorage["user"];
-//     document.getElementById("user").value = user;
-//     alert("you are already a user")
-// }
-// else {
-//     document.getElementById("user").placeholder = "What will you be foing today";
-//     console.log("user no found in localStorage")
-// }
-// document.getElementById("saveBtn").addEventListener("click", function () {
-//     var user = document.getElementById("user").value;
-//     //localStorage["user"] = user ;
-//     localStorage.setItem("user", user);
-//     alert("your task has been updated");
-//     console.log("click")
-// }, false);
+// currentTextarea.addClass("present")
+// currentTextarea.addClass("Fut")
+
+}
+
+});
 
 
 
-
-let newArray = [];
-let time = moment();
-let timeFormat = "hh:mma"
-let eightStart = moment('8:00am', timeFormat)
-let eightEnd = moment('8:59am', timeFormat)
-let nineStart = moment('9:00am', timeFormat)
-let nineEnd = moment('9:59am', timeFormat)
-let tenStart = moment('10:00am', timeFormat)
-let tenEnd = moment('10:59am', timeFormat)
-let elevenStart = moment('11:00am', timeFormat)
-let elevenend = moment('11:59am', timeFormat)
-let twelveStart = moment('12:00am', timeFormat)
-let oneStart = moment('1:00pm', timeFormat)
-let oneEnd = moment('1:59pm', timeFormat)
-let twoStart = moment('2:00pm', timeFormat)
-let twoEnd = moment('2:59pm', timeFormat)
-let threeStart = moment('3:00pm', timeFormat)
-let threeEnd = moment('3:59pm', timeFormat)
-let fourStart = moment('4:00pm', timeFormat)
-let fourEnd = moment('4:59pm', timeFormat)
-let fiveStart = moment('5:00pm', timeFormat)
-let fiveEnd = moment('5:59pm', timeFormat)
-
-setInterval(function () {
-    checkTime();
-}, 1000);
-
-// function checkTime() {
-//     if (time.isBefore(eightStart)) {
-//         $('user').addClass('future')
-//     } else if (time.isBetween(eightStart, eightEnd)) {
-//         $('user').addClass('present')
-//     } else if (time.isAfter(eightEnd)) {
-//         $('user').addClass('past')
-//     }
-// }
-
-// iso string
-// m = moment("2020/08/18 2:32PM", "DD/MM/YYYY");
-
-// the dates are in the log
-// console.log(m.year());
-// console.log(m.toString());;
-// console.log(m.toISOString());
-});   
-   
